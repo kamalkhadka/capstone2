6a45520c68e28687f986699d5ccba70177b33820
Kamal Khadka <kamalkhadka@Kamals-MacBook-Pro.local>
2021-08-17 00:50:40 -0500

    updated dashboard and footer

--- dividend-tracker/src/Footer.js
+++ dividend-tracker/src/Footer.js
@@ -1,18 +1,13 @@
-import { Col, Container, Row } from "react-bootstrap";
 export default function Footer() {
   return (
-    <div className="main-footer">
-      <Container>
-        <Row>
-          <Col className="col-md-3 col-sm-6">
-            <h4>Portfolio Builder</h4>
-            <ul>
-              <li>hello</li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <footer>
+      <div class="container">
+        <div class="row text-center">
+          <span class="copyright quick-links">Copyright &copy; Portfolio Builder
+          </span>
+        </div>
+      </div>
+    </footer>
   );
 }
 
--- dividend-tracker/src/containers/Dashboard.js
+++ dividend-tracker/src/containers/Dashboard.js
@@ -1,17 +1,82 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row, Table } from "react-bootstrap";
 
-export default function Dashboard(){
+export default function Dashboard() {
     return (
-        <Row className="border">
-            <Col>
-                Column 1
-            </Col>
-            <Col>
-                Column 2
-            </Col>
-            <Col>
-                Column 3
-            </Col>
-        </Row>
+        <>
+            <Row className="mb-3">
+                <Col>
+                    <Card>
+                        <Card.Body>$51,130.69 <br />Value
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>+$252.63 (0.49%) <br />Day's Gain/Loss
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>+$2,948.78 <br /> Gain/Loss
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+
+            <Button className="mb-3" variant="primary">Add Stock</Button>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Security</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Cost</th>
+                        <th>Gain/Loss</th>
+                        <th>Value</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>APPL <br /> Apple Inc</td>
+                        <td>151.12</td>
+                        <td>70</td>
+                        <td>8933.86</td>
+                        <td>+1660</td>
+                        <td>10594</td>
+                        <td><a href="/buy">Buy</a> | <a href="/sell">Sell</a></td>
+
+                    </tr>
+                    <tr>
+                        <td>MSFT <br /> Microsoft Corp</td>
+                        <td>151.12</td>
+                        <td>70</td>
+                        <td>8933.86</td>
+                        <td>+1660</td>
+                        <td>10594</td>
+                        <td><a href="/buy">Buy</a> | <a href="/sell">Sell</a></td>
+
+                    </tr>
+                    <tr>
+                        <td>JPM <br /> JPMorgan Chase &amp; Co</td>
+                        <td>151.12</td>
+                        <td>70</td>
+                        <td>8933.86</td>
+                        <td>+1660</td>
+                        <td>10594</td>
+                        <td><a href="/buy">Buy</a> | <a href="/sell">Sell</a></td>
+                        
+                    </tr>
+                    <tr>
+                        <td colSpan="3">Totals</td>
+                        <td>1000</td>
+                        <td>2000</td>
+                        <td>3000</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
     );
 }
